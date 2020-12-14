cd1478ea571381d5c94c0eedb02cdea58c5cb2c8
ViolaCello <ViolaCelloNYC@gmail.com>
2020-12-14 15:06:25 -0500

    POST working, created process_trade in the Model to do the math for the trade instance

--- trade-journal-backend/app/models/trade.rb
+++ trade-journal-backend/app/models/trade.rb
@@ -1,2 +1,15 @@
 class Trade < ApplicationRecord
+
+
+    def process_trade
+        if self.profit >= 0 
+            self.result = true
+        else
+            self.result = false
+        end
+        self.ror = self.profit.to_f / self.risk.to_f
+      
+    end
+
+
 end
