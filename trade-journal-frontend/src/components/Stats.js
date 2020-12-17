debaeeb83087745c0c63256e3a04dfe010d068dd
ViolaCello <ViolaCelloNYC@gmail.com>
2020-12-17 15:32:15 -0500

    toggle button on Strategy Page now works correctly

--- trade-journal-frontend/src/components/Stats.js
+++ trade-journal-frontend/src/components/Stats.js
@@ -5,30 +5,45 @@ import Stocks from './Stocks'
 class Stats extends React.Component {
 
 state = {
-    stat : true,
+    stat : false,
     stok : false
 }
 
 
 toggle = () => {
-    console.log("Hi")
+    console.log("Hi", this.state)
    if (this.state.stat) {
      return (
    <div> <Strategy trades={this.props.trades} /> </div>
       )
    }
-       else {
-           return <div> <Stocks trades={this.props.trades} /> </div>
+       else if (this.state.stok){
+           return (<div> <Stocks trades={this.props.trades} /> </div> )
        }
    }
 
    handleClick = (event) => {
-       console.log("handleClick:", event)
-       this.setState({
-           stat: false,
-           stok: true
-        }
-       )}
+       console.log("handleClick:", event.target.name)
+       switch(event.target.name) {
+       case "stratButton":
+           console.log("STRATTTTTTTT", event.target.name==="stratButton")
+       this.setState((state)=>{
+        return   {
+        stat: true,
+           stok: false}
+        } )
+        break;
+        case "stockButton":
+            console.log("NOOOOOOOO")
+            this.setState((state)=>{
+                return   {
+                stat: false,
+                   stok: true}
+                } )
+             default: 
+             return null
+       }
+    }
    
 
 render() {
