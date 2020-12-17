cf44cc4cef1124ac135977686e9f8d557c1c5c0f
ViolaCello <ViolaCelloNYC@gmail.com>
2020-12-17 10:55:48 -0500

    cleaned up tradeReducer for unused functions

--- trade-journal-frontend/src/reducers/tradesReducer.js
+++ trade-journal-frontend/src/reducers/tradesReducer.js
@@ -12,17 +12,17 @@ export default function tradesReducer(state = {
         case "GET_TRADES":
           //  console.log("Doing Math", action.payload[0].profit)
          // debugger
-            let profitsArray = []
-            action.payload.map(array => profitsArray.push(array.profit))
-            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            // let profitsArray = []
+            // action.payload.map(array => profitsArray.push(array.profit))
+            // const reducer = (accumulator, currentValue) => accumulator + currentValue;
             // console.log("reducer: ", profitsArray.reduce(reducer))
-            let strategyArray = []
-            action.payload.map(array => strategyArray.push(array.strategy))
+            // let strategyArray = []
+            // action.payload.map(array => strategyArray.push(array.strategy))
             // let uniqStrategies = [...new Set(strategyArray)]
             return {...state, trades: action.payload,
-               totalTrades: action.payload.length,
-                totalProfit: profitsArray.reduce(reducer).toFixed(2),
-                uniqStrategies: [...new Set(strategyArray)]
+              //  totalTrades: action.payload.length,
+              //   totalProfit: profitsArray.reduce(reducer).toFixed(2),
+              //   uniqStrategies: [...new Set(strategyArray)]
              }
 
              case 'ADD_TRADE_SUCCESS':
