0e718d670365a8e2a2d67b0c10d817d2748b68ff
ViolaCello <ViolaCelloNYC@gmail.com>
2020-12-20 12:58:00 -0500

    created Welcome component

---
+++ trade-journal-frontend/src/components/Welcome.js
@@ -0,0 +1,24 @@
+ const Welcome = () => {
+     return (
+         <div>
+             <p>Welcome to your Trade Journal.</p>
+             <p>The first step towards your profitable success as a trader is to keep an accurate record of what is working for you as a trader, and more importantly, what ISN'T working for you as a trader.</p>
+            <br /><br />
+            <p><b>To Use:</b></p>
+            <ol>
+                <li>Click <b>Enter New Trade</b></li>
+                <li>Enter the details of the trade, including:<br />
+                Security Ticker Symbol<br />
+                Date of Trade <br />
+                Strategy user for this trade *** This is most important, as the way to learn to trade is to document what you're doing to make a profit.<br />
+                You can choose a strategy that you have perviously used from the dropdown menu, or you click "New Strategy" and enter a new strategy.  </li>
+                Also enter the amount of profit or loss (enter losses with a negative number) from the trade outcome.
+            </ol>
+            <p>After some trade data is enter, click <b>Stats</b> to see your overall statistics, as well as profit broken down by Stock and Strategy</p>
+            <p>Visit the <b>Trades</b> page to view a list of your trades.  You can click on a particular trade to get further information on the trade, as well as to Edit it or Delete it from the database.</p>
+
+         </div>
+     )
+ }
+
+ export default Welcome
